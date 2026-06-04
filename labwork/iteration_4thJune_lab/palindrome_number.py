ca1aa0933175248fd032bff533e8cf4fdbbe48e4
Manvendra Singh Rajput <rajputmanvendra58@gmail.com>
2026-06-04 15:04:07 +0530

    Added to check reverse number and palindrome number
    Added comments to clarify the logic for reversing the number.

--- labwork/iteration_4thJune_lab/palindrome_number.py
+++ labwork/iteration_4thJune_lab/palindrome_number.py
@@ -4,7 +4,9 @@ original = num
 reverse = 0
 # Reverse the number
 while num > 0:
+    #to check the last digit
     digit = num % 10
+    #adding the last digit
     reverse = reverse * 10 + digit
     num = num // 10
 print("Reverse Number:", reverse)
