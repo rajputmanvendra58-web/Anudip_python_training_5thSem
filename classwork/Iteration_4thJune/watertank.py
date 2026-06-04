f5395ece2614f6a0940487c8135c6e216971727b
Manvendra Singh Rajput <rajputmanvendra58@gmail.com>
2026-06-04 11:49:49 +0530

    Filling of Water Tank

--- classwork/Iteration_4thJune/watertank.py
+++ classwork/Iteration_4thJune/watertank.py
@@ -1,6 +1,6 @@
 #Program for water tank filling
-water_level=0
-while(water_level<100):
+water_level=0   #initial water level is 0
+while(water_level<100): #the tank will fill upto 100 litres
     water_level+=10
-    print("Water is filling..Current water level: ",water_level,"Liters")
+    print("Water is filling..Current water level: ",water_level,"Litres")
 print("Water tank is Full.")
