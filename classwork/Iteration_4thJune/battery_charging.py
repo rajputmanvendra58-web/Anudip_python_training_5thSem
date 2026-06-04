658d7a256ab5bd93b8abca1031599fe41538744b
Manvendra Singh Rajput <rajputmanvendra58@gmail.com>
2026-06-04 13:58:05 +0530

    iteration lab works

--- classwork/Iteration_4thJune/battery_charging.py
+++ classwork/Iteration_4thJune/battery_charging.py
@@ -1,9 +1,9 @@
 #Program for displaying battery charging status
-charging_level = 20
-electricity_status = input("Is the electricity available? (yes/no):")
+charging_level = 20 #initial charging level is 20%
+electricity_status = input("Is the electricity available? (yes/no):")  # to check if there is electricity or not?
 if(electricity_status.lower()!="yes"):
     exit("Electricity is not available. Cannot charge the battery.")
 while(charging_level<100):
     print("Battery is charging level:", charging_level, "%")
     charging_level+=10
-print("Battery is fully charged")
+print("Battery is fully charged")     #displaying all the battery level
