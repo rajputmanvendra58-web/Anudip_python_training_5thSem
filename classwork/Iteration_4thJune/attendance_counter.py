9f52498e6aa8c9f4600b989d3f3e6738d3740a7e
Manvendra Singh Rajput <rajputmanvendra58@gmail.com>
2026-06-04 12:02:51 +0530

    Displayin the attendance of every entered student

---
+++ classwork/Iteration_4thJune/attendance_counter.py
@@ -0,0 +1,9 @@
+#Program to count the entered students
+count = 0
+
+while count < 30:
+    print("Student Entered")     #Entry of each and every student
+    count += 1
+    print("Attendance Count:", count)  #Attendacne of all students counting
+
+print("All students are present.")  #Displaying the students attendance
