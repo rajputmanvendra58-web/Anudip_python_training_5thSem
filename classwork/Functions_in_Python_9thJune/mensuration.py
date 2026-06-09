2af5b8a27a9ec8c3281d675a640e851309f1e998
Manvendra Singh Rajput <rajputmanvendra58@gmail.com>
2026-06-09 14:58:14 +0530

    functions for area and perimeter

---
+++ classwork/Functions_in_Python_9thJune/mensuration.py
@@ -0,0 +1,21 @@
+#create functions for area and perimeter for 2 d figurues
+# function to calculate area of rectangle
+def rectangle_area(length, width):
+    return length * width
+# function to calculate perimeter of Rectangle
+def rectangle_perimeter(length, width):
+    return 2 * (length + width)
+#---------------------------------------------------
+# # function to calculate area of Square
+def square_area(side):
+    return side * side
+# function to calculate perimeter of Square
+def square_perimeter(side):
+    return 4 * side
+#-----------------------------------------------------
+# function to calculate area of circle
+def circle_area(radius):
+    return 3.14 * radius * radius
+# function to calculate perimeter of circle
+def circle_perimeter(radius):
+    return 2 * 3.14 * radius
